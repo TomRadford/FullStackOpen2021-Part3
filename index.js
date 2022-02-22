b4cf482e6390cdbdb3eb969a17b7570bc990650f
Tom Radford <tom@theradford.com>
2022-02-22 09:27:48 +0200

    exercises up to  3.8

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express()
 app.use(express.json())
 
 
-
-app.use(morgan('tiny '))
+morgan.token('content', function (req , res) {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons =
     [
